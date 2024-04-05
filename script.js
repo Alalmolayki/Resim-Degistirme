540a2d61a1c693980a499803393b56e0f59f36a4
Ala Almolayki <136168933+Alalmolayki@users.noreply.github.com>
2024-04-05 11:30:52 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+const fill = document.querySelector('.fill');
+const empty = document.querySelector('.empty')
+window.onload = function() {
+    let resimUrl = "https://source.unsplash.com/random/300x300";
+    fill.style.backgroundImage = "url('" + resimUrl + "')";
+    
+    
+
+
+}
